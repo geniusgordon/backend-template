4f539bfd2dc73aff54ba09b5000801834400f96b
Gordon <geniusgordon@gmail.com>
2016-05-12 17:02:54 +0800

    dbUrl NODE_ENV

--- src/core/config.js
+++ src/core/config.js
@@ -1,7 +1,13 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 export const port = process.env.PORT || 3000;
 export const host = process.env.HOST || `localhost:${port}`;
 
-export const dbUrl = process.env.DB_URL || 'mongodb://localhost/backend';
+export const dbUrl = {
+  production: process.env.DB_DEV_URL || 'mongodb://localhost/backend',
+  development: process.env.DB_DEV_URL || 'mongodb://localhost/backend-dev',
+  test: process.env.DB_TEST_URL || 'mongodb://localhost/backend-test',
+};
 
 export const jwtSecret = process.env.JWT_SECRET || 'SECRET';
 
