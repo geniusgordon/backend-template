34be4277f1e67a5fd4b824765a6b25a1f88736bd
Gordon <geniusgordon@gmail.com>
2016-07-15 03:26:09 +0800

    add stage-0

--- src/core/db.js
+++ src/core/db.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { dbUrl } from './config';
 
+mongoose.Promise = Promise;
+
 export function connectDb() {
   return new Promise((resolve, reject) => {
     mongoose.connect(dbUrl[process.env.NODE_ENV], (err) => {
